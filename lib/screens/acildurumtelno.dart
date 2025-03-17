520939f2ed037ba29224fb7a0530b25cbe4983fb
busrabetus <poslubusrab@gmail.com>
2025-03-17 15:08:20 +0300

    acildurumno sayfası eklendi

--- lib/screens/acildurumtelno.dart
+++ lib/screens/acildurumtelno.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:izukbs/screens/sinavsonuclari.dart';
 import 'package:izukbs/screens/sinavtakvimi.dart';
 import 'package:izukbs/screens/transkript.dart';
@@ -17,11 +18,20 @@ class acildurumtelno extends StatefulWidget {
   State<acildurumtelno> createState() => _acildurumtelnoState();
 }
 
+String? selectedRelation;
+String? selectedGender;
+TextEditingController nameController = TextEditingController();
+TextEditingController surnameController = TextEditingController();
+TextEditingController noController = TextEditingController();
+
+
 class _acildurumtelnoState extends State<acildurumtelno> {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor:  Color(0xFFF0F0F0),
+      backgroundColor: Color(0xFFF0F0F0),
       appBar: AppBar(
         backgroundColor: Color(0xFF8B2231),
         title: Text("Acil Durum İletişim Telefonu", style: TextStyle(color: Colors.white)),
@@ -150,25 +160,157 @@ class _acildurumtelnoState extends State<acildurumtelno> {
       ),
       body: Stack(
         children: [
-        Align(
-        alignment: Alignment.center,
-        child: Image.asset(
-          'assets/images/circles.png',
-          width: 400,
-          height: 400,
-          fit: BoxFit.cover,
-        ),
+          Align(
+            alignment: Alignment.center,
+            child: Image.asset(
+              'assets/images/circles.png',
+              width: 400,
+              height: 400,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height:40),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: Row(
+                      children: [
+                        SizedBox(width: 7,),
+                        Icon(Icons.phone),
+                        Text(" Acil Durum Telefon Numarası Ekle",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ]
+                  ),
+                ),
+                SizedBox(height: 5),
+                Container(
+                  width: 400,
+                  height: 300,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(
+                          color: Colors.black.withOpacity(0.1),
+                          width: 0.01
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          blurRadius: 10,
+                          offset: Offset(4, 4),
+                        )
+                      ]
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Yakınlık Derecesi: ",
+                              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(width: 20,),
+                            DropdownButton<String>(
+                              value: selectedRelation,
+                              hint: Text("Seçiniz"),
+                              onChanged: (String? newValue) {
+                                setState(() {
+                                  selectedRelation = newValue;
+                                });
+                              },
+                              items: ["Anne", "Baba", "Kardeş"].map((String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }).toList(),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: TextField(
+                                controller: nameController,
+                                decoration: InputDecoration(
+                                  labelText: 'Ad',
+                                  border: OutlineInputBorder(),
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                            Expanded(
+                              child: TextField(
+                                controller: surnameController,
+                                decoration: InputDecoration(
+                                  labelText: 'Soyad',
+                                  border: OutlineInputBorder(),
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                            // Cinsiyet Dropdown
+                            DropdownButton<String>(
+                              value: selectedGender,
+                              hint: Text("Cinsiyet"),
+                              onChanged: (String? newValue) {
+                                setState(() {
+                                  selectedGender = newValue;
+                                });
+                              },
+                              items: ["Kadın", "Erkek"].map((String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }).toList(),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        Text("Acil durum kişisi cep telefon numarası",
+                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 7),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: TextField(
+                                controller: noController,
+                                keyboardType: TextInputType.number,
+                                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                                decoration: InputDecoration(
+                                  labelText: '(___) ___ ____ ',
+                                  border: OutlineInputBorder(),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-
-
-
-          // SAYFA İCERİGİ BURADA OLACAK
-
-
-
-
-      ]
-      )
     );
   }
 }
