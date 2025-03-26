7a847ddfc28c39f9e5b289aa604a3b5976c939ea
Esra Nur Yiğit <031021039@std.izu.edu.tr>
2025-03-26 22:25:03 +0300

    devamsizlik sayfasi guncellendi

--- lib/screens/devamsizlikdurumu.dart
+++ lib/screens/devamsizlikdurumu.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:izukbs/drawer.dart';
 
-class devamsizlikdurumu extends StatefulWidget {
-  const devamsizlikdurumu({super.key});
+class DevamsizlikDurumu extends StatefulWidget {
+  const DevamsizlikDurumu({super.key});
 
   @override
-  State<devamsizlikdurumu> createState() => _devamsizlikdurumuState();
+  State<DevamsizlikDurumu> createState() => _DevamsizlikDurumuState();
 }
 
-class _devamsizlikdurumuState extends State<devamsizlikdurumu> {
+class _DevamsizlikDurumuState extends State<DevamsizlikDurumu> {
+  // Örnek devamsızlık verileri
+  final List<Map<String, dynamic>> devamsizlikListesi = [
+    {'ders': 'Algoritmaya Giriş', 'devamsizlik': 30},
+    {'ders': 'Yapay Zeka', 'devamsizlik': 69.888777},
+    {'ders': 'Programlamaya Giriş', 'devamsizlik': 76.3333},
+    {'ders': 'Üniversiteyi Anlamak ve Eleştirel Düşünme', 'devamsizlik': 65.3333},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor:  Color(0xFFF0F0F0),
+      backgroundColor: Color(0xFFF0F0F0),
       appBar: AppBar(
         backgroundColor: Color(0xFF8B2231),
         title: Text("Devamsızlık Durumu", style: TextStyle(color: Colors.white)),
@@ -20,27 +28,50 @@ class _devamsizlikdurumuState extends State<devamsizlikdurumu> {
         iconTheme: IconThemeData(color: Colors.white),
       ),
       drawer: drawer(),
-        body: Stack(
-            children: [
-              Align(
-                alignment: Alignment.center,
-                child: Image.asset(
-                  'assets/images/circles.png',
-                  width: 400,
-                  height: 400,
-                  fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Image.asset(
+              'assets/images/circles.png',
+              width: 400,
+              height: 400,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Ders Devamsızlık Bilgileri",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
-              ),
-
-
-
-              // SAYFA İCERİGİ BURADA OLACAK
-
-
-
-
-            ]
-        )
+                SizedBox(height: 10),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: devamsizlikListesi.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        elevation: 3,
+                        margin: EdgeInsets.symmetric(vertical: 8),
+                        child: ListTile(
+                          title: Text(devamsizlikListesi[index]['ders']),
+                          trailing: Text(
+                            "${devamsizlikListesi[index]['devamsizlik']} %",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
