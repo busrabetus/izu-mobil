6e474f91dcbc42a0bb0cbd395fd8ea714f9c579c
busrabetus <poslubusrab@gmail.com>
2025-05-11 14:44:30 +0300

    tokenservice update

--- lib/services/token_service.dart
+++ lib/services/token_service.dart
@@ -1,29 +1,39 @@
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:jwt_decode/jwt_decode.dart';
 
 class AuthService {
   static const String _tokenKey = 'token';
 
-  // Token'ı kaydeder
   static Future<void> saveToken(String token) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString(_tokenKey, token);
   }
 
-  // Token'ı getirir
   static Future<String?> getToken() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getString(_tokenKey);
   }
 
-  // Token'ı siler
   static Future<void> removeToken() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.remove(_tokenKey);
   }
 
-  // Kullanıcı giriş yapmış mı kontrol eder
   static Future<bool> isLoggedIn() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.containsKey(_tokenKey);
+    final token = prefs.getString(_tokenKey);
+
+    if (token == null) return false;
+
+    try {
+      if (Jwt.isExpired(token)) {
+        await removeToken();
+        return false;
+      }
+      return true;
+    } catch (e) {
+      await removeToken();
+      return false;
+    }
   }
 }
